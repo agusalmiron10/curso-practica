ced4ea5618fd5f9f8582821a7435df187fadd6b8
agusalmiron10 <agustinalmiron32@gmail.com>
2023-04-14 15:14:50 -0300

    modificacion index

--- Sistur/react.js
+++ Sistur/react.js
@@ -11,11 +11,18 @@ loginForm.addEventListener('submit', function(event) {
         console.log(data);
         const token = data.token;
         document.cookie = token;
+        window.location.href = 'index1.html';  //esto te redirige hacia el hola .html
     })
     .catch(error => {
         console.log(error);
     });
 });
+
+
+$(document).ready(function () {
+    $('#dtBasicExample').DataTable();
+    $('.dataTables_length').addClass('bs-select');
+    });
 
 
 
